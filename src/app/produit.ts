e053fb26d4f3c35fadb8535219a28bba8e10682a
Marwen Helali <marwen.helali@gmail.com>
2020-11-23 23:22:07 +0100

    shop-cart

---
+++ src/app/produit.ts
@@ -0,0 +1,69 @@
+export const prodact = [
+    {
+        prodName: "Café noisette",
+        price: 4.2,
+        imge: "Café noisette",
+        description: "Préparation : 1 min Navrée pour les gourmands en quête de saveur insolite mais le café noisette n'est qu'un café auquel a été ajouté une goutte de lai"
+    },
+    {
+        prodName: "Capsule",
+        price: 5.5,
+        imge: "capsul",
+        description: "Caps Me est une marque de capsules réutilisables qui réconcilie l’écologie et la qualité sans sacrifier la simplicité de se faire un café."
+    },
+    {
+        prodName: "Café au lait",
+        price: 3.5,
+        imge: "Café au lait",
+        description: ""
+    },
+    {
+        prodName: "Café creme",
+        price: "",
+        imge: "creme",
+        description: "Le rapport de recherche mondial « De Crème À Café » propose des expériences subjectives et quantitatives correspondant au taux de développement de l’industrie"
+    },
+    {
+        prodName: "Café glacée",
+        price: 4.2,
+        imge: "glacee",
+        description: "essayez de faire du café glacé avec de la vanille ou un café glacé frappé !"
+    },
+    {
+        prodName: "Café turc",
+        price: 3.5,
+        imge: "turk",
+        description: "Le café turc est le café oriental connu dans le monde entier pour son goût puissant unique et sa texture épaisse et onctueuse. Jamais égalé,"
+    },
+    {
+        prodName: "Café pistache",
+        price: 4.8,
+        imge: "pictag",
+        description: "Disposez de la ganache de coco montée, puis de la ganache de pistache. Ajoutez quelques pistaches torréfiées, une quenelle de glace au café"
+    },
+    {
+        prodName: "cafe tiramisu san francisco",
+        price: 5.4,
+        imge: "tirami",
+        description: "Pour ceux qui aiment le café, c'est un dessert sympa, frais et qui passe bien même quand on n'a plus faim"
+    },
+    {
+        prodName: "Café filtre",
+        price: 2.5,
+        imge: "filtre",
+        description: "Le café filtre est un café aromatique et plein de goût : c'est comme cela que les Américains et les Européens du Nord l'aiment"
+    },
+    {
+        prodName: "Café Mocha",
+        price: 3.8,
+        imge: "mocha",
+        description: "Découvrez la recette de Café Mocha à faire en 10 minutes. Faire couler un espresso, plus il sera serré mieux ce sera, soit 2 cl. Dans une tasse, verser une cuillère à café et demie de chocolat en poudre."
+    },
+    {
+        prodName: "café frappé",
+        price: 4.7,
+        imge: "café frappé",
+        description: "Le café frappé, très tendance en été, a envahi les cafés branchés des grandes villes.Et pourtant, il n’a pourtant rien de nouveau. Préparé avec du café instantané,"
+    },
+  
+]
--- src/app/shopp-cart/shopp-cart.component.ts
+++ src/app/shopp-cart/shopp-cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { prodact } from '../produit';
 
 @Component({
   selector: 'app-shopp-cart',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopp-cart.component.css']
 })
 export class ShoppCartComponent implements OnInit {
+
+  prodactList = prodact;
 
   constructor() { }
 
